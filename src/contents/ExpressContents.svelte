3e1375c22daf469dc9fc71987c9152c8bfb1fb20
Luthfi Aji W <luthfi.ajiw@gmail.com>
2020-12-02 07:27:08 +0700

    fix typo express content

--- src/contents/ExpressContents.svelte
+++ src/contents/ExpressContents.svelte
@@ -10,9 +10,9 @@ import ListTile from "./components/ListTile.svelte";
             category="Restful API"
             delay=""
             description="API ini digunakan pada aplikasi Bankboo, aplikasi yang juga masih saya develop sendiri. Aplikasi ini merupakan aplikasi pelayanan bank sampah secara mobile. Sistemnya masih cukup sederhana, dimana ada user yang bisa mendaftar menjadi nasabah suatu bank
-            kemudian setelah itu bisa melakukan transaksi setor sampah yang akan dikonversi menjadi saldo uang dan juga bisa melakukan transaksi penarikan saldo. Transaksi yang terjadi harus melibatkan kedua pihak, yaitu ketika nasabah melakukan transaksi maka pihak bank akan men-scan QR code transaksi milik nasabah tersebut untuk memverifikasinya.
-            Validasi pada API ini masih memiliki banyak celah karena memang ini adalah API percobaan pertama saya. Dan karena saya seorang frontend developer jadi saya menggunakan ORM untuk membantu saya berinteraksi dengan database, ORM yang digunakan adalah Sequelize. MVP untuk saat ini hanya sekedar bisa melakukan transaksi, 
-            selanjutnya saya akan melanjutkan developing aplikasi mobile."
+            setelah itu bisa melakukan transaksi setor sampah yang akan dikonversi menjadi saldo uang dan juga bisa melakukan transaksi penarikan saldo. Transaksi yang terjadi harus melibatkan kedua belah pihak, yaitu ketika nasabah melakukan transaksi maka pihak bank akan men-scan QR code transaksi milik nasabah tersebut untuk memverifikasinya.
+            Validasi pada API ini masih memiliki banyak celah karena memang ini adalah API percobaan pertama saya. Dan karena saya seorang frontend developer jadi saya menggunakan ORM supaya terbantu dalam berinteraksi dengan database, ORM yang digunakan pada API ini adalah Sequelize. MVP untuk saat ini hanya sekedar bisa melakukan transaksi, 
+            selanjutnya saya akan melanjutkan developing dari sisi aplikasi mobile."
         />
     </div>
 </div>
