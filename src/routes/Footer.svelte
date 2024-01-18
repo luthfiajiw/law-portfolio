7626f60e329fcd92c4f6662a320875f9f76ea6f1
Luthfi Aji Wicaksono <41244004+luthfiajiw@users.noreply.github.com>
2024-01-18 08:44:31 +0700

    remove footer animation

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -18,16 +18,15 @@
   on:exitViewport={() => handleVisibility(false)}
   class="footer mt-4 mb-2 px-4 pt-5"
 >
-  {#if visible}
+  
     <div class="row justify-content-between">
-      <p style="color: rgba(255, 255, 255, 0.5);" transition:fly="{{ x: -100, duration: 500 }}">
+      <p style="color: rgba(255, 255, 255, 0.5);">
         Made with <img src="../favicon.png" alt="svelte" class="svelte-logo" width="20px"> and <i class="fas fa-mug-hot"></i>
       </p>
-      <div style="color: rgba(255, 255, 255, 0.5);" transition:fly="{{ x: 100, duration: 500 }}">
+      <div style="color: rgba(255, 255, 255, 0.5);">
         <i class="fas fa-copyright"></i> 2023
       </div>
     </div>
-  {/if}
 </div>
 
 <style>
